ef1a94ae73bf21341d3a20885c4c5dee2e4dc021
AN <abakumova.n03@gmail.com>
2024-11-10 23:06:58 +0700

    Initial commit

---
+++ test_file.py
@@ -0,0 +1,8 @@
+import main
+import pytest
+
+
+def test_result():
+    bank = main.BankInterest(100000, 2, 10)
+    assert len(bank.ann_int()) == 2
+    assert bank.ann_int() == (4614.49, 110747.82)
